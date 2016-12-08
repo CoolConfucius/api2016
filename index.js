bfb7e316f4b4e35c30e8b2cba5dc3e56b253073f
Benjamin Huang <coolconfucius@yahoo.com>
2016-12-07 16:40:18 -0800

    start

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var path = require('path');
+// Create an Express App
+var app = express();
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./config/mongoose.js');
+
+var routes_setter = require('./config/routes.js');
+routes_setter(app);
+
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
